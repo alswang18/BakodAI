ad1eff8dc2ebd01e20e209aa8d529d4563f86139
Alec Wang <alswang18@gmail.com>
2020-10-03 00:08:17 -0700

    finished upload functionality

--- bakod/inference/views.py
+++ bakod/inference/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+def index(request):
+    context={'a':1}
+    return render(request, 'upload.html', context)
+
+def predictImage(request):
+    print(request.POST.dict())
+    print(request.FILES['filePath'])
+
+    fileObj= request.FILES['filePath']
+    fs = FileSystemStorage()
+    filePathName=fs.save(fileObj.name,fileObj)
+    filePathName=fs.url(filePathName)
+
+    context={'filePathName':filePathName}
+    return render(request, 'upload.html', context)
