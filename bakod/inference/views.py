1732d865d269ad2ea741a93fdbcd76de27dbfe19
Alec Wang <alswang18@gmail.com>
2020-10-03 01:30:44 -0700

    added predictIMage stuff

--- bakod/inference/views.py
+++ bakod/inference/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
 import json
-img_height, img_width= 128,128
+img_height, img_width=128,128
+classes=['agriculture','artisinal_mine','bare_ground','blooming','blow_down','clear',
+'cloudy','conventional_mine','cultivation','habitation','haze',
+'partly_cloudy','primary','road','selective_logging','slash_burn','water']
+
 def index(request):
     context={'a':1}
     return render(request, 'upload.html', context)
@@ -15,6 +19,13 @@ def predictImage(request):
     fs = FileSystemStorage()
     filePathName=fs.save(fileObj.name,fileObj)
     filePathName=fs.url(filePathName)
-
-    context={'filePathName':filePathName}
+    testimage='.'+filePathName
+    context={'filePathName':filePathName,'predictedLabel':predictedLabel}
     return render(request, 'upload.html', context)
+
+def viewDataBase(request):
+    import os
+    listOfImages=os.listdir('./media/')
+    listOfImagesPath=['./media/'+i for i in listOfImages]
+    context={'listOfImagesPath':listOfImagesPath}
+    return render(request,'viewDB.html',context) 
