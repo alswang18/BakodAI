ff17685c2d3f22ca2834d75746f062b21b6c9339
tanzellgo <tanzell.go@obf.ateneo.edu>
2020-10-16 11:48:48 +0800

    img preview solved for real

--- bakod/bakod/settings.py
+++ bakod/bakod/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '4gh@1n78$!qd9%_)#6!%)91!+5+omwq*+)wv=pti6)#6op!k0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
